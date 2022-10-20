bbca9d059f7f6b8f532dbae4b88595c1a23b0da3
SadieEdwar <sadie.edwards@my.uwi.edu>
2022-10-20 20:51:54 +0000

    added build files

---
+++ build/esp8266/source/chip_boot.d
@@ -0,0 +1,6 @@
+source/chip_boot.o: \
+ /opt/esp_sdk/ESP8266_RTOS_SDK/components/esp8266/source/chip_boot.c \
+ /home/sadieedwards/esp/lab1_q2/gpio/build/include/sdkconfig.h
+
+/home/sadieedwards/esp/lab1_q2/gpio/build/include/sdkconfig.h:
+/opt/esp_sdk/ESP8266_RTOS_SDK/components/esp8266/source/chip_boot.c:
---
+++ build/heap/src/esp_heap_trace.d
@@ -0,0 +1,6 @@
+src/esp_heap_trace.o: \
+ /opt/esp_sdk/ESP8266_RTOS_SDK/components/heap/src/esp_heap_trace.c \
+ /home/sadieedwards/esp/lab1_q2/gpio/build/include/sdkconfig.h
+
+/home/sadieedwards/esp/lab1_q2/gpio/build/include/sdkconfig.h:
+/opt/esp_sdk/ESP8266_RTOS_SDK/components/heap/src/esp_heap_trace.c:
